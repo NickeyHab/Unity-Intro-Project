1e5e4d7dab6cbaa0297201d4a0d7c67e82d03fed
NickeyHab <35151942+NickeyHab@users.noreply.github.com>
2025-04-18 23:35:40 +0200

    feature: created floor reset trigger

---
+++ Assets/Scripts/FloorResetTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorResetTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"));
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+    }
+}
